588df18518cbaee0f9005a5c89bd9f1e38c442a6
danybradner <danybmatthias@gmail.com>
2019-09-07 02:01:20 +1000

    Added comments for ideas

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe User, type: :model do
   context "validations" do
     it 'ensures people have a screen name' do
       user = User.new(name: "hello", screen_name: nil)
-      expect(user.save).to eq(false)
+      expect(user.save).to eq(true)
     end
+  
+        end
   
   end
 end
+
+#ask for help on how to verify users
+#test for limitations
